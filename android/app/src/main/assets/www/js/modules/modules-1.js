8affcee20e9d137ba8b382987e941dc20efe9a05
matheusstanlei12-dotcom <matheus.stanlei12@gmail.com>
2026-06-17 11:57:02 -0300

    fix: remove stray closing div tag that broke equipment detail modal layout

--- android/app/src/main/assets/www/js/modules/modules-1.js
+++ android/app/src/main/assets/www/js/modules/modules-1.js
@@ -428,7 +428,6 @@ window.EquipmentModule = (() => {
           <div class="modal-title" id="eq-detail-title">Detalhes</div>
           <button class="modal-close" onclick="closeModal('modal-eq-detail')"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"/></svg></button>
         </div>
-        </div>
         <div class="modal-body" id="eq-detail-body"></div>
       </div>
     </div>
